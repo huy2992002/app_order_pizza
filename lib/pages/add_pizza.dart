7b818c343dea0fd14e9b536f4481aed2bf44047f
dqhoangg2k2 <hoang123pcc8@gmail.com>
2023-12-04 18:03:40 +0700

    add pizza to cart

---
+++ lib/pages/add_pizza.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPizzaPage extends StatefulWidget {
+  const AddPizzaPage({super.key});
+
+  @override
+  State<AddPizzaPage> createState() => _AddPizzaPageState();
+}
+
+class _AddPizzaPageState extends State<AddPizzaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
